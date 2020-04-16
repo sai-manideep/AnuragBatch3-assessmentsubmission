da96fbd04b1e9adbf7c699d93e66c12317795188
aruna <arunajava567@gmail.com>
2020-04-16 18:19:36 +0530

    Second commit -2

--- TrainerrManagementTrainer/src/main/java/com/cg/TrainerrManagementTrainer/App.java
+++ TrainerrManagementTrainer/src/main/java/com/cg/TrainerrManagementTrainer/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("springBoot Backend app");
+        System.out.println(" welcome to Angualr integartion ");
     }
 }
