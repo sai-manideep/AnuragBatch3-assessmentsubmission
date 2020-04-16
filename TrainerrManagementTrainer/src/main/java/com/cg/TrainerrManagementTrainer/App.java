23ba84e49038474257687ee5ac9d6707ffa4cc99
aruna <arunajava567@gmail.com>
2020-04-16 18:16:57 +0530

    First commit -1

---
+++ TrainerrManagementTrainer/src/main/java/com/cg/TrainerrManagementTrainer/App.java
@@ -0,0 +1,14 @@
+package com.cg.TrainerrManagementTrainer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("springBoot Backend app");
+    }
+}
